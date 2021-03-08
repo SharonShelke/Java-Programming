8c8c01bba039d9d792c9ba14fb50ae7cbe34e4e4
SharonShelke <78440482+SharonShelke@users.noreply.github.com>
2021-03-09 01:45:19 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,15 @@
+package Practise;
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            // statements
+
+            System.out.println("i=" + i);
+            i++;
+
+        } while (i <= 4); // condition check;
+    }
+
+}
+
